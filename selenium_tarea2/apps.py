ee3f81511a1c817bedfb95b50bce4afe8ae68b8a
amarianjel <abraham.marianjel@gmail.com>
2023-05-25 16:58:53 -0400

    Solucion en setting agregando la direccion

--- selenium_tarea2/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SeleniumTarea2Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'selenium_tarea2'
--- web/apps.py
+++ web/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RequestTarea1Config(AppConfig):
+class WebConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'request_tarea1'
+    name = 'web'
